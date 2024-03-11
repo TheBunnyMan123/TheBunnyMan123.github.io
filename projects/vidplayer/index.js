cb001bd35bafb9606eb82fb54613b464012e7218
TheBunnyMan123 <bunny@thekillerbunny.com>
2024-03-10 23:35:40 -0500

    add volume slider

--- projects/vidplayer/index.js
+++ projects/vidplayer/index.js
@@ -2,11 +2,18 @@ var juice = document.querySelector('.playback-juice');
 var btn = document.getElementById('playPause');
 var playImage = document.getElementById('playPauseImage');
 var vid = document.getElementById("videoPlayer");
+var volume = document.getElementById("volumeSlider");
 
 video.addEventListener("change", function() {
   var media = window.URL.createObjectURL(this.files[0]);
   vid.src = media;
 });
+
+volume.oninput = function() {
+  vid.volume = (this.value / 100);
+}
+
+vid.volume = volume.value;
 
 function playPause() {
   if (vid.paused) {
